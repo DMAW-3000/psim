40aeb351f4903f7e08c3d64fb547a66c4fd339e6
DMAW-3000 <thewood005@gmail.com>
2025-04-01 12:19:52 -0400

    Restore PL/M sources to build

--- app_mds80/mkapps.bat
+++ app_mds80/mkapps.bat
@@ -2,12 +2,12 @@ rem del cstat.hex
 rem del madiag.hex
 del msbasic.hex
 
-rem python ..\..\pyplm\pyplm.py -o -e extern_cpm.asm -s STATUS cstat.plm cstat.asm
-rem ..\..\asw\bin\asw -g -cpu 8080 cstat.asm
-rem ..\..\asw\bin\p2hex -F Intel cstat.p
+python ..\..\pyplm\pyplm.py -o -e extern_cpm.asm -s STATUS cstat.plm cstat.asm
+..\..\asw\bin\asw -g -cpu 8080 cstat.asm
+..\..\asw\bin\p2hex -F Intel cstat.p
 
-rem ..\..\asw\bin\asw -g -cpu 8080 madiag.asm
-rem ..\..\asw\bin\p2hex -F Intel madiag.p
+..\..\asw\bin\asw -g -cpu 8080 madiag.asm
+..\..\asw\bin\p2hex -F Intel madiag.p
 
-..\..\asw\bin\asw -g -cpu 8080 msbasic.asm
-..\..\asw\bin\p2hex -F Intel msbasic.p
+rem ..\..\asw\bin\asw -g -cpu 8080 msbasic.asm
+rem ..\..\asw\bin\p2hex -F Intel msbasic.p
