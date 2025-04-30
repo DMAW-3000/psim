9a5791fc5230c34f50a40b5e79bfb53779e09061
DMAW-3000 <thewood005@gmail.com>
2025-04-30 16:08:28 -0400

    Add shifted keys

--- applecon.py
+++ applecon.py
@@ -13,6 +13,7 @@ class APPLECON_App(object):
         self._kbSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._kbAddr = ('127.0.0.1', args.port)
         self._kbData = bytearray(1)
+        self._kbShift = False
         
         self._port = shared_memory.SharedMemory("VMEM_APPLE1", True, 1)
         self._vram = bytearray(4096)
@@ -24,7 +25,7 @@ class APPLECON_App(object):
         self._cgreen = pygame.Color(0, 255, 0)
         self._cred   = pygame.Color(255, 0, 0)
         
-        self._kb_map = {
+        self._kb_map_lower = {
             pygame.K_1          : ord('1'),
             pygame.K_2          : ord('2'),
             pygame.K_3          : ord('3'),
@@ -45,6 +46,62 @@ class APPLECON_App(object):
             pygame.K_COMMA      : ord(','),
             pygame.K_PERIOD     : ord('.'),
             pygame.K_SLASH      : ord('/'),
+            pygame.K_SPACE      : ord(' '),
+            
+            pygame.K_q          : ord('q'),
+            pygame.K_w          : ord('w'),
+            pygame.K_e          : ord('e'),
+            pygame.K_r          : ord('r'),
+            pygame.K_t          : ord('t'),
+            pygame.K_y          : ord('y'),
+            pygame.K_u          : ord('u'),
+            pygame.K_i          : ord('i'),
+            pygame.K_o          : ord('o'),
+            pygame.K_p          : ord('p'),
+            pygame.K_a          : ord('a'),
+            pygame.K_s          : ord('s'),
+            pygame.K_d          : ord('d'),
+            pygame.K_f          : ord('f'),
+            pygame.K_g          : ord('g'),
+            pygame.K_h          : ord('h'),
+            pygame.K_j          : ord('j'),
+            pygame.K_k          : ord('k'),
+            pygame.K_l          : ord('l'),
+            pygame.K_z          : ord('z'),
+            pygame.K_x          : ord('x'),
+            pygame.K_c          : ord('c'),
+            pygame.K_v          : ord('v'),
+            pygame.K_b          : ord('b'),
+            pygame.K_n          : ord('n'),
+            pygame.K_m          : ord('m'),
+            
+            pygame.K_TAB        : 15,
+            pygame.K_RETURN     : 0x0d,
+            
+            pygame.K_BACKQUOTE  : ord('`'),
+        }
+        
+        self._kb_map_upper = {
+            pygame.K_1          : ord('!'),
+            pygame.K_2          : ord('@'),
+            pygame.K_3          : ord('#'),
+            pygame.K_4          : ord('$'),
+            pygame.K_5          : ord('%'),
+            pygame.K_6          : ord('^'),
+            pygame.K_7          : ord('&'),
+            pygame.K_8          : ord('*'),
+            pygame.K_9          : ord('('),
+            pygame.K_0          : ord(')'),
+            
+            pygame.K_ESCAPE     : 0x1b,
+            pygame.K_MINUS      : ord('-'),
+            pygame.K_EQUALS     : ord('='),
+            pygame.K_BACKSPACE  : 0x5f,
+            pygame.K_SEMICOLON  : ord(':'),
+            pygame.K_BACKSLASH  : ord('|'),
+            pygame.K_COMMA      : ord('<'),
+            pygame.K_PERIOD     : ord('>'),
+            pygame.K_SLASH      : ord('?'),
             pygame.K_SPACE      : ord(' '),
             
             pygame.K_q          : ord('Q'),
@@ -98,11 +155,21 @@ class APPLECON_App(object):
         
         while True:
             for event in pygame.event.get():
-                if (event.type == pygame.KEYDOWN):
-                    if event.key in self._kb_map:
-                        self._kbData[0] = self._kb_map[event.key]
+                if event.type == pygame.KEYDOWN:
+                    if (event.key == pygame.K_LSHIFT) or (event.key == pygame.K_RSHIFT):
+                        self._kbShift = True
+                        continue
+                    if self._kbShift:
+                        kmap = self._kb_map_upper
+                    else:
+                        kmap = self._kb_map_lower
+                    if event.key in kmap:
+                        self._kbData[0] = kmap[event.key]
                         self._kbSock.sendto(self._kbData, self._kbAddr)
                         time.sleep(0)
+                elif event.type == pygame.KEYUP:
+                    if (event.key == pygame.K_LSHIFT) or (event.key == pygame.K_RSHIFT):
+                        self._kbShift = False
                 elif event.type == pygame.QUIT:
                     sys.exit()
                     
